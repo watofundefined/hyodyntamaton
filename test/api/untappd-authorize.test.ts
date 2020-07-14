60ef1d18dc4bee5865b288f5d7a86103175fa311
Matej Duracka <watofundefined@o.o0.0>
2020-07-14 10:10:12 +0300

    test: WIP Selective mocking of axios requests
    The problem: I want to test app API endpoints which are calling external
    endpoints.
    
    I tried mswjs lib - a http interceptor library, but I had troubles with
    CORS in my current setup. It seemed to drop http headers for requests
    that where supposed to bypass it.

--- test/api/untappd-authorize.test.ts
+++ test/api/untappd-authorize.test.ts
@@ -1,23 +1,56 @@
 import http, { Server, IncomingMessage, ServerResponse } from 'http'
-import axios from 'axios'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
 import { apiResolver } from 'next/dist/next-server/server/api-utils'
 
 import handler from '../../pages/api/untappd-authorize'
 
-const apiContext = {
-  previewModeId: '',
-  previewModeEncryptionKey: '',
-  previewModeSigningKey: '',
-}
+jest.mock('axios', () => {
+  const originalModule = jest.requireActual('axios')
+  const originalGet = originalModule.get
+
+  function get(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<any>> {
+    switch (url) {
+      case process.env.NEXT_PUBLIC_UNTAPPD_AUTHORIZE_URL: {
+        return new Promise((resolve, reject) => {
+          if (config.params.code == process.env.TEST_VALID_UNTAPPD_AUTH_CODE) {
+            return resolve({
+              status: 200,
+              data: { response: { access_token: process.env.TEST_UNTAPPD_ACCESS_TOKEN } },
+              statusText: 'ok',
+              headers: {},
+              config: {} as any,
+            })
+          }
+
+          reject({
+            status: 400,
+            data: { error: 'failed authentication - invalid code' },
+          })
+        })
+      }
+      default: {
+        return originalGet(url, config)
+      }
+    }
+  }
+
+  return {
+    ...originalModule,
+    get,
+  }
+})
 
 let server: Server
 
 beforeEach(() => {
-  let requestHandler = (req: IncomingMessage, res: ServerResponse) => {
+  server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
-    return apiResolver(req, res, {}, handler, apiContext)
-  }
-  server = http.createServer(requestHandler)
+    return apiResolver(req, res, {}, handler, {
+      previewModeId: '',
+      previewModeEncryptionKey: '',
+      previewModeSigningKey: '',
+    })
+  })
   server.listen({ port: process.env.PORT })
 })
 
@@ -33,10 +66,29 @@ afterEach((done) => {
 })
 
 describe('/api/untappd-authorize handler', () => {
-  test("responds 400 to GET without any 'code' query param", async () => {
+  test('responds 400 when no code is passed', async () => {
     expect.assertions(1)
     return axios
       .get(`http://localhost:${process.env.PORT}/`)
       .catch((e) => expect(e.response.status).toBe(400))
+  })
+
+  test('responds 400 when invalid code is used', async () => {
+    expect.assertions(1)
+    return axios
+      .get(`http://localhost:${process.env.PORT}?code=not-a-valid-code`)
+      .catch((e) => expect(e.response.status).toBe(400))
+  })
+
+  test('responds 200 and token when valid code is used', async () => {
+    expect.assertions(2)
+    return axios
+      .get(
+        `http://localhost:${process.env.PORT}?code=${process.env.TEST_VALID_UNTAPPD_AUTH_CODE}`
+      )
+      .then((r) => {
+        expect(r.status).toBe(200)
+        expect(r.data.token).toBe(process.env.TEST_UNTAPPD_ACCESS_TOKEN)
+      })
   })
 })
--- test/setup-env.js
+++ test/setup-env.js
@@ -1,3 +1,13 @@
-// Make sure there are no port conflicts when instantiating server
+
+// Untappd
+process.env.NEXT_PUBLIC_UNTAPPD_AUTHENTICATE_URL = 'http://untappd.auth.xyz'
+process.env.NEXT_PUBLIC_UNTAPPD_AUTHORIZE_URL = 'http://untappd.authorize.xyz'
+process.env.NEXT_PUBLIC_UNTAPPD_API_URL = 'http://untappd.api.xyz'
+process.env.NEXT_PUBLIC_UNTAPPD_CLIENT_ID = 'fake-client-id'
+
+// Test-only
+process.env.TEST_VALID_UNTAPPD_AUTH_CODE = 'a-valid-auth-code'
+process.env.TEST_UNTAPPD_ACCESS_TOKEN = 'untappd-access-token'
+
 const port = 8800 + Number(process.env.JEST_WORKER_ID)
 process.env.PORT = process.env.PORT || port.toString()
