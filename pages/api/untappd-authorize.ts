ccc32893bd8de26510a98ebb3fd41741a855605b
Matej Duracka <watofundefined@o.o0.0>
2020-07-14 10:09:45 +0300

    refactor: untappd-authorize api

--- pages/api/untappd-authorize.ts
+++ pages/api/untappd-authorize.ts
@@ -1,46 +1,57 @@
-import querystring from 'querystring'
 import { NextApiRequest, NextApiResponse } from 'next'
-import axios from 'axios'
+import axios, { AxiosResponse, AxiosRequestConfig } from 'axios'
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const queryString = req.url.split('?')[1]
-  const { code } = querystring.parse(queryString)
-
-  if (!code) {
-    res.statusCode = 400
-    res.json({
-      error:
-        "Couldn't complete Untappd authorization process. " +
-        'Wrong response from Untappd server.',
-    })
-    return
-  }
-
-  try {
-    const untappdRes = await axios.get(
-      process.env.NEXT_PUBLIC_UNTAPPD_AUTHORIZE_URL,
-      {
-        params: getRequestParams(req.headers.referer, code),
-      }
-    )
-
-    res.statusCode = 200
-    res.json({ token: untappdRes.data.response.access_token })
-  } catch (error) {
-    res.statusCode = 400
-    res.json({ error })
-  }
+interface AuthorizeResponse {
+  error?: string
+  token?: string
 }
 
-function getRequestParams(referer: string, code: string | string[]): object {
+interface UntappdResponse {
+  response: { access_token: string }
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<AuthorizeResponse>) => {
+  const code = req.query.code as string
+
+  if (!code) {
+    return error(res, "Couldn't complete Untappd authorization process. Missing code param.")
+  }
+
+  let authRes: AxiosResponse<UntappdResponse>
+
+  try {
+    authRes = await axios.get(
+      process.env.NEXT_PUBLIC_UNTAPPD_AUTHORIZE_URL,
+      reqConfig(req.headers.referer, code)
+    )
+  } catch (err) {
+    return error(res, err)
+  }
+
+  success(res, { token: authRes.data.response.access_token })
+}
+
+function success(res: NextApiResponse, data) {
+  res.statusCode = 200
+  res.json(data)
+}
+
+function error(res: NextApiResponse, message: string) {
+  res.statusCode = 400
+  res.json({ error: message })
+}
+
+function reqConfig(referer: string, code: string): AxiosRequestConfig {
   const url = new URL(referer)
 
   return {
-    code,
-    response_type: 'code',
-    client_id: process.env.NEXT_PUBLIC_UNTAPPD_CLIENT_ID,
-    client_secret: process.env.UNTAPPD_CLIENT_SECRET,
-    redirect_url: url.origin + '/auth',
+    params: {
+      code,
+      response_type: 'code',
+      client_id: process.env.NEXT_PUBLIC_UNTAPPD_CLIENT_ID,
+      client_secret: process.env.UNTAPPD_CLIENT_SECRET,
+      redirect_url: url.origin + '/auth',
+    },
   }
 }
 
