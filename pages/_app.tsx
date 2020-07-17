494b5e1d7bbe965f8b9f5b849f070375732b4919
Matej Duracka <watofundefined@o.o0.0>
2020-07-17 15:00:33 +0300

    feat: add client-side mocking with msw (missed file)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-
-import StaticHead from '../components/static-head'
-import { reducer, getInitialState } from '../state'
-
-import '../styles/index.scss'
 import AuthValidator from '../components/auth-validator'
+import StaticHead from '../components/static-head'
+import { getInitialState, reducer } from '../state'
+import '../styles/index.scss'
 
 // Logic to handle SSR, grab token from localStorage only on the client - duh.
 // This causes Next.js to throw a warning when user already has a token,
@@ -14,8 +12,8 @@ import AuthValidator from '../components/auth-validator'
 // (Warning is 'Expected server HTML to contain a matching text node...')
 // The only way I see this go away is if I'd use cookies or different routes.
 // Ignoring for now.
-const token =
-  typeof document != 'undefined' ? localStorage.getItem('token') : null
+const isBrowser = typeof document != 'undefined'
+const token = isBrowser ? localStorage.getItem('token') : null
 
 const store = createStore(
   reducer,
@@ -28,6 +26,14 @@ const store = createStore(
 )
 
 const publicRoutes = ['/', '/auth', '/about']
+
+if (
+  isBrowser &&
+  process.env.NODE_ENV === 'development' &&
+  process.env.NEXT_PUBLIC_MOCKING_ENABLED === 'true'
+) {
+  require('../mocks/browser').default.start()
+}
 
 function App({ Component: Page, pageProps }: AppProps) {
   return (
