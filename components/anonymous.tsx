68e7b482b11456138372f9f68fd357e2375fa5cb
Matej Duracka <watofundefined@o.o0.0>
2020-08-03 12:32:27 +0300

    feature: improve the login screen (mention 'beer')

--- components/anonymous.tsx
+++ components/anonymous.tsx
@@ -9,7 +9,7 @@ function Anonymous(props: AnonymousProps): JSX.Element {
         <header>
           <span className="logo">Hyödyntämätön</span>
         </header>
-        <span>Simple app built on top of Untappd and Foursquare APIs.</span>
+        <span>Simple beer app built on top of Untappd and Foursquare APIs.</span>
         <span>Work in progress.</span>
         <button className="btn" onClick={props.onLoginClicked}>
           Log in with your Untappd account
