9f1bd704a8e1309c29cd47e2b00d652734ac7b41
Matej Duracka <watofundefined@o.o0.0>
2020-08-24 16:11:50 +0300

    feat: disable zoom

--- components/static-head.tsx
+++ components/static-head.tsx
@@ -5,7 +5,10 @@ function StaticHead() {
     <Head>
       <title>Hyödyntämätön</title>
       <link rel="icon" href="/favicon.ico" />
-      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+      />
       <link
         href="https://fonts.googleapis.com/css2?family=Staatliches&display=block"
         rel="stylesheet"
